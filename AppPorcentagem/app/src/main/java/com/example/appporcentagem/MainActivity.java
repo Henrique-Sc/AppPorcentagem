a0a49078b9115c26e2493cbabf53f2d5c61cc821
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-11 19:45:40 -0300

    Update MainActivity.java

--- AppPorcentagem/app/src/main/java/com/example/appporcentagem/MainActivity.java
+++ AppPorcentagem/app/src/main/java/com/example/appporcentagem/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.appporcentagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtNum;
+    Button btn5, btn10, btn15, btn35;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNum = findViewById(R.id.edtNum);
+
+        btn5 = findViewById(R.id.btn5);
+        btn10 = findViewById(R.id.btn10);
+        btn15 = findViewById(R.id.btn15);
+        btn35 = findViewById(R.id.btn35);
     }
 }
